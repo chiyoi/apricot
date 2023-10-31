d60e7c0a978ab16c9ae1410133169992972156ce
chiyoi <chiyoi2140@outlook.com>
2023-10-31 16:28:03 +0800

    update: Add level.

--- logs/logs.go
+++ logs/logs.go
@@ -5,83 +5,125 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
 )
 
-var std = NewLoggers()
+var std = func() (std *Loggers) {
+	std = NewLoggers()
+	std.callDepth = 3
+	return
+}()
 
 func SetOutput(w io.Writer) {
 	std.SetOutput(w)
+}
+
+func SetLevel(l Level) {
+	std.SetLevel(l)
 }
 
 func Prefix(prefix string) {
 	std.Prefix(prefix)
 }
 
+func Debug(v ...any) {
+	std.Debug(v...)
+}
+
 func Info(v ...any) {
-	std.i.Output(2, fmt.Sprintln(v...))
+	std.Info(v...)
 }
 
 func Warning(v ...any) {
-	std.w.Output(2, fmt.Sprintln(v...))
+	std.Warning(v...)
 }
 
 func Error(v ...any) {
-	std.e.Output(2, fmt.Sprintln(v...))
-}
-
-func Debug(v ...any) {
-	std.d.Output(2, fmt.Sprintln(v...))
+	std.Error(v...)
 }
 
 func Panic(v any) {
-	std.p.Output(2, fmt.Sprintln(v))
-	panic(v)
+	std.Panic(v)
 }
 
 type Loggers struct {
-	i, w, e, p, f, d *log.Logger
+	mu        sync.RWMutex
+	level     Level
+	loggers   map[Level]*log.Logger
+	callDepth int
 }
+
+type Level int
+
+const (
+	LevelDebug Level = iota
+	LevelInfo
+	LevelWarning
+	LevelError
+	LevelPanic
+)
 
 func NewLoggers() *Loggers {
 	return &Loggers{
-		i: log.New(os.Stderr, "(info) ", log.Lmsgprefix|log.LstdFlags|log.LUTC),
-		w: log.New(os.Stderr, "(warning) ", log.Lmsgprefix|log.Lshortfile|log.LstdFlags|log.LUTC),
-		e: log.New(os.Stderr, "(error) ", log.Lmsgprefix|log.Llongfile|log.LstdFlags|log.LUTC),
-		p: log.New(os.Stderr, "(panic) ", log.Lmsgprefix|log.Llongfile|log.LstdFlags|log.LUTC),
-		f: log.New(os.Stderr, "(fatal) ", log.Lmsgprefix|log.LstdFlags|log.LUTC),
-		d: log.New(os.Stderr, "(debug) ", log.Lmsgprefix|log.Llongfile|log.LstdFlags|log.LUTC),
+		level: LevelInfo,
+		loggers: map[Level]*log.Logger{
+			LevelInfo:    log.New(os.Stderr, "(info) ", log.Lmsgprefix|log.LstdFlags|log.LUTC),
+			LevelWarning: log.New(os.Stderr, "(warning) ", log.Lmsgprefix|log.Lshortfile|log.LstdFlags|log.LUTC),
+			LevelError:   log.New(os.Stderr, "(error) ", log.Lmsgprefix|log.Llongfile|log.LstdFlags|log.LUTC),
+			LevelPanic:   log.New(os.Stderr, "(panic) ", log.Lmsgprefix|log.Llongfile|log.LstdFlags|log.LUTC),
+			LevelDebug:   log.New(os.Stderr, "(debug) ", log.Lmsgprefix|log.Llongfile|log.LstdFlags|log.LUTC),
+		},
+		callDepth: 2,
 	}
 }
 
 func (ls *Loggers) SetOutput(output io.Writer) {
-	for _, l := range []*log.Logger{ls.i, ls.w, ls.e, ls.p, ls.f, ls.d} {
+	ls.mu.Lock()
+	defer ls.mu.Unlock()
+	for _, l := range ls.loggers {
 		l.SetOutput(output)
 	}
 }
 
+func (ls *Loggers) SetLevel(l Level) {
+	ls.mu.Lock()
+	defer ls.mu.Unlock()
+	ls.level = l
+}
+
 func (ls *Loggers) Prefix(prefix string) {
-	for _, l := range []*log.Logger{ls.i, ls.w, ls.e, ls.p, ls.f, ls.d} {
+	for _, l := range ls.loggers {
 		l.SetPrefix(prefix + " " + l.Prefix())
 	}
 }
 
+func (ls *Loggers) Debug(v ...any) {
+	if ls.level <= LevelDebug {
+		ls.loggers[LevelDebug].Output(ls.callDepth, fmt.Sprintln(v...))
+	}
+}
+
 func (ls *Loggers) Info(v ...any) {
-	ls.i.Output(2, fmt.Sprintln(v...))
+	if ls.level <= LevelInfo {
+		ls.loggers[LevelInfo].Output(ls.callDepth, fmt.Sprintln(v...))
+	}
 }
 
 func (ls *Loggers) Warning(v ...any) {
-	ls.w.Output(2, fmt.Sprintln(v...))
+	if ls.level <= LevelWarning {
+		ls.loggers[LevelWarning].Output(ls.callDepth, fmt.Sprintln(v...))
+	}
 }
 
 func (ls *Loggers) Error(v ...any) {
-	ls.e.Output(2, fmt.Sprintln(v...))
-}
-
-func (ls *Loggers) Debug(v ...any) {
-	ls.d.Output(2, fmt.Sprintln(v...))
+	if ls.level <= LevelError {
+		ls.loggers[LevelError].Output(ls.callDepth, fmt.Sprintln(v...))
+	}
 }
 
 func (ls *Loggers) Panic(v any) {
-	ls.p.Output(2, fmt.Sprintln(v))
+	if ls.level <= LevelPanic {
+		ls.loggers[LevelPanic].Output(ls.callDepth, fmt.Sprintln(v))
+	}
 	panic(v)
 }

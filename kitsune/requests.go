fb404b42f3ee791f1d42d9b46cba3763b0056585
chiyoi <chiyoi2140@outlook.com>
2023-11-27 23:36:51 +0800

    fix

--- kitsune/requests.go
+++ kitsune/requests.go
@@ -45,7 +45,7 @@ func GetStream(ctx context.Context, hook res.Hook[*http.Request]) func(u string)
 	}
 }
 
-func PostJSON(ctx context.Context, resp, hook res.Hook[*http.Request], req any) func(u string) (err error) {
+func PostJSON(ctx context.Context, resp any, hook res.Hook[*http.Request], req any) func(u string) (err error) {
 	return func(u string) (err error) {
 		body, err := JSONReader(req)
 		r, err := res.R(body, err, runnerNewRequestWithContext(ctx, http.MethodPost, u))

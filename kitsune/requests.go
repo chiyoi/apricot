1b6aac6ad9b51236ccf6bca9f47f6a259d5eb8a9
chiyoi <chiyoi2140@outlook.com>
2023-12-01 21:01:49 +0800

    tidy

--- kitsune/requests.go
+++ kitsune/requests.go
@@ -15,21 +15,7 @@ func JSONReader(a any) (r io.Reader, err error) {
 	return &buf, json.NewEncoder(&buf).Encode(a)
 }
 
-func Get(ctx context.Context, hook res.Hook[*http.Request]) func(u string) (body io.ReadCloser, err error) {
-	return func(u string) (body io.ReadCloser, err error) {
-		r, err := http.NewRequestWithContext(ctx, http.MethodGet, u, nil)
-		if hook != nil {
-			r, err = res.R(r, err, hook)
-		}
-		re, err := res.R(r, err, http.DefaultClient.Do)
-		if err != nil {
-			return
-		}
-		return re.Body, ParseResponse(re, nil)
-	}
-}
-
-func GetJSON(ctx context.Context, resp any, hook res.Hook[*http.Request]) func(u string) error {
+func Get(ctx context.Context, resp any, hook res.Hook[*http.Request]) func(u string) error {
 	return func(endpoint string) (err error) {
 		r, err := http.NewRequestWithContext(ctx, http.MethodGet, endpoint, nil)
 		if hook != nil {
@@ -42,6 +28,20 @@ func GetJSON(ctx context.Context, resp any, hook res.Hook[*http.Request]) func(u
 		defer re.Body.Close()
 		err = ParseResponse(re, resp)
 		return
+	}
+}
+
+func GetStream(ctx context.Context, hook res.Hook[*http.Request]) func(u string) (body io.ReadCloser, err error) {
+	return func(u string) (body io.ReadCloser, err error) {
+		r, err := http.NewRequestWithContext(ctx, http.MethodGet, u, nil)
+		if hook != nil {
+			r, err = res.R(r, err, hook)
+		}
+		re, err := res.R(r, err, http.DefaultClient.Do)
+		if err != nil {
+			return
+		}
+		return re.Body, ParseResponse(re, nil)
 	}
 }
 
@@ -58,25 +58,6 @@ func Post(ctx context.Context, body io.Reader, resp any, hook res.Hook[*http.Req
 		}
 		defer re.Body.Close()
 		return ParseResponse(re, resp)
-	}
-}
-
-func PostJSON(ctx context.Context, resp any, hook res.Hook[*http.Request]) func(req any) func(u string) (err error) {
-	return func(req any) func(u string) (err error) {
-		return func(u string) (err error) {
-			body, err := JSONReader(req)
-			r, err := res.R(body, err, runnerNewRequestWithContext(ctx, http.MethodPost, u))
-			if hook != nil {
-				r, err = res.R(r, err, hook)
-			}
-			r, err = res.R(r, err, runnerSetHeaderLine("Content-Type", "application/json"))
-			re, err := res.R(r, err, http.DefaultClient.Do)
-			if err != nil {
-				return
-			}
-			defer re.Body.Close()
-			return ParseResponse(re, resp)
-		}
 	}
 }
 
@@ -108,12 +89,6 @@ func Delete(ctx context.Context, hook res.Hook[*http.Request]) func(u string) (e
 		}
 		defer re.Body.Close()
 		return ParseResponse(re, nil)
-	}
-}
-
-func runnerNewRequestWithContext(ctx context.Context, method string, u string) func(body io.Reader) (*http.Request, error) {
-	return func(body io.Reader) (*http.Request, error) {
-		return http.NewRequestWithContext(ctx, method, u, body)
 	}
 }
 
